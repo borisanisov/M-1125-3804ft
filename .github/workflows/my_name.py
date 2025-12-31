c46bed49e2db7932c31b0e7cd24bbda89d44a79f
borisanisov <boris009anisov@gmail.com>
2025-12-31 15:44:41 +1000

    Добавлен Python скрипт и GitHub Actions workflow

---
+++ .github/workflows/my_name.py
@@ -0,0 +1,10 @@
+def main():
+    full_name = "Анисов Борис Кириллович"
+    group_number = "М1125-38.04.05фт" 
+    
+    print("=" * 50)
+    print(f"ФИО: {full_name}")
+    print(f"Номер группы: {group_number}")
+    print("=" * 50)
+
+main()
